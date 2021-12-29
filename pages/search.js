9f342e1359c5070bcc99f437c4ba99ec4ab6e727
AryanCodr <codr.aryan@gmail.com>
2021-12-29 15:57:17 +0530

    making a google clone

---
+++ pages/search.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import Header from '../components/Header'
+
+function Search() {
+    return (
+        <div>
+            <Head>
+                <title>Search Result</title>
+                <link rel="icon" href="logo.svg" />
+            </Head>
+
+            {/* Header */}
+            <Header />
+            {/* Search Results */}
+        </div>
+    )
+}
+
+export default Search
